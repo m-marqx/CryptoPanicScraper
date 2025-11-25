a60c07770d4e9d469d98ddeb6208361bfc459d6b
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2025-11-24 22:46:59 -0500

    refactor(main): initialize CryptoPanicScraper with parameters

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import asyncio
 from api_news_scraper import CryptoPanicScraper
 
 async def main():
-    scraper = CryptoPanicScraper()
-    scraper.filter = "all"
-    scraper.limit = 50
-    scraper.topic = "BTC"
+    scraper = CryptoPanicScraper(
+        filter='all',
+        limit=10000,
+        # topic='BTC',
+        save_path='news_data',
+        # jina_api_key='jina_4d7479b1a43542138c177e3e3a76f7c7RgVhcuusmPIdTaGzSVumpW_Zf3Gi',
+        max_retries=5,
+    )
+
     await scraper.run()
 
 if __name__ == "__main__":
